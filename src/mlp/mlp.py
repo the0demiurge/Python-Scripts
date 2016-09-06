a8c8c653e9c192a3a43fd63ae89a33a4e67977b9
the0demiurge <the0demiurge@gmail.com>
2016-09-06 21:03:51 +0800

    updated .gitignore

--- src/mlp/mlp.py
+++
@@ -1,6 +0,0 @@
-from pylab import *
-from sklearn import preprocessing
-t = linspace(1,10)
-x = mat((3*t*t+rand(1, len(t)), 2*t+rand(1, len(t)), -t+rand(1, len(t)), 1/t)+rand(1, len(t)))
-y = t / 10.0
-
