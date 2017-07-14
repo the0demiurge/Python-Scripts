5ca5d0b987a4f14dd6328dfc22344d5098c6d0d9
Charles Xu <charl3s.xu@gmail.com>
2017-07-14 10:25:25 +0800

    added ipgwlogin

---
+++ src/DevTools/jupyter_hide_code.py
@@ -0,0 +1,15 @@
+from IPython.display import import HTML
+
+HTML(''<script>
+     code_show=true; 
+     function code_toggle() {
+      if (code_show){
+       $('div.input').hide();
+       } else {
+        $('div.input').show();
+        }
+      code_show = !code_show
+     } 
+     $( document ).ready(code_toggle);
+     </script>
+     <form action="javascript:code_toggle()"><input type="submit" value="Click here to toggle on/off the raw code."></form>'')')
---
+++ src/Web/ipgw.py
@@ -0,0 +1,66 @@
+import requests
+import time
+import re
+import os
+import bs4
+
+help = '''东北大学IP网关登陆器
+用法：
+    {name} query 查询状态
+    {name} login username password 登陆网关
+    {name} logout username password 退出网关
+'''.format(name=__file__)
+
+
+def query():
+    # 查询状态
+    payload = {
+        'action': 'get_online_info',
+    }
+    r = requests.post('http://ipgw.neu.edu.cn/include/auth_action.php', data=payload)
+    return r.content.decode('utf-8')
+
+
+def login(username='', password=''):
+    # 登陆
+    # E2620 已经在线
+    # E2616 已欠费
+    # E2531 用户不存在
+    # E2553 密码错误
+    # E2606 用户被禁用
+
+    # '1. 请' 成功登陆
+    payload = {
+        'action': 'login',
+        'ac_id': '1',
+        'user_ip': '',
+        'nas_ip': '',
+        'user_mac': '',
+        'url': '',
+        'username': '{}'.format(username),
+        'password': '{}'.format(password),
+        'save_me': '0'}
+    r = requests.post('http://ipgw.neu.edu.cn/srun_portal_pc.php', data=payload)
+    soup = bs4.BeautifulSoup(r.content.decode('utf-8'), 'html.parser')
+    if re.findall('5分钟', str(soup.p)):
+        return 'sleep', str(soup.p)
+    if str(soup.p)[3:7] == '1. 请':
+        return 'True', str(soup.p)
+    else:
+        return str(soup.p)[3:8], str(soup.p)
+
+
+def logout(username='', password=''):
+    # 断开连接
+    payload = {
+        'action': 'logout',
+        'ajax': '1',
+        'username': '{}'.format(username),
+        'password': '{}'.format(password)}
+    r = requests.post('http://ipgw.neu.edu.cn/include/auth_action.php', data=payload)
+    return r.text
+
+
+if __name__ == '__main__':
+    import fire
+    fire.Fire()
