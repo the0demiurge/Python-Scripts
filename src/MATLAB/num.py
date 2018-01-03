e28b66a17b55aebef7cf8dda99fd9a3af1c87891
Charles Xu <charl3s.xu@gmail.com>
2018-01-03 15:25:37 +0800

    changed form to numpy and sympy

--- src/MATLAB/num.py
+++ src/MATLAB/num.py
@@ -2,6 +2,7 @@
 """Python MATLAB style tools
 """
 import numpy as np
+from math import sqrt
 
 inv = np.linalg.inv
 rank = np.linalg.matrix_rank
@@ -9,6 +10,10 @@ det = np.linalg.det
 exp = np.linalg.matrix_power
 
 
-def mat(data, shape):
+def mat(data, shape=None):
     # Make a matrix by inputing data in one-line and mat-shape
-    return np.mat(np.reshape(data, shape[::-1]))
+    if not shape:
+        shape = [round(sqrt(len(data)))] * 2
+    elif isinstance(shape, int):
+        shape = [shape, len(data) / shape]
+    return np.mat(np.reshape(data, shape))
---
+++ src/MATLAB/sym.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Python MATLAB style tools
+"""
+import sympy as s
+from math import sqrt
+from sympy import symbols, Matrix, MatrixSymbol, abc, det, Inverse
+
+inv = s.Inverse
+exp = s.MatPow
+
+
+def mat(data, shape=None):
+    # Make a matrix by inputing data in one-line and mat-shape
+    if not shape:
+        shape = [round(sqrt(len(data)))] * 2
+    elif isinstance(shape, int):
+        shape = [shape, len(data) / shape]
+    return Matrix(s.reshape(data, [shape[0]]))
