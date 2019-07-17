8d81d5a9d5a3cb884e2b94709ba3358d0e7b42a8
Charles Xu <charl3s.xu@gmail.com>
2019-07-17 17:28:08 +0800

    updated tieba_content.py

--- src/MinorProjects/tieba_content.py
+++ src/MinorProjects/tieba_content.py
@@ -1,17 +1,42 @@
+import os
+
+import bs4
+import requests
+
+
 def extract_content(url):
-    html = requests.get(url).text
-    soup = bs4.BeautifulSoup(html, 'lxml')
-    contents = soup.find_all('div', attrs={'class': 'p_content'})[1:]
-    return soup.title, '\n'.join(map(str,contents))
+    url = url.strip()
+    html = requests.get(url, params={'see_lz': 1}).text
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    contents = soup.find_all('div', attrs={'class': 'p_content'})
+    # contents = contents[1:]
+    return soup.title.text, '\n'.join(map(str, contents))
 
 
+def crawl(url_list):
+    head = str(bs4.BeautifulSoup(requests.get(url_list[0]).text, 'html.parser').head)
+    result_list = [head]
+    result_list.append('<body>')
+    for url in url_list:
+        print(url, end='\t')
+        title, content = extract_content(url)
+        print(title)
+        result_list.append('<h1>{}</h1>'.format(title))
+        result_list.append(content)
+    result_list.append('</body>')
+    result = '\n'.join(result_list)
+    return result
 
 
-result  = [str(soup.head)]
-result.append('<body>')
-for hh, ccc in zip(header, cc):
-    result.append('<h1>{}</h1>'.format(hh))
-    result.append(ccc)
-result.append('</body>')
-r = '\n'.join(result)
+def main():
+    path = 'url_list.txt'
+    if not os.path.exists(path):
+        print('need', path)
+        exit(0)
+    urls = [i for i in open(path).readlines() if len(i) > 5]
+    content = crawl(urls)
+    open('tieba.html', 'w').write(content)
 
+
+if __name__ == '__main__':
+    main()
