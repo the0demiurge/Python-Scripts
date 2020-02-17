7037faf003bbea2151455347b6fb8e8f0f66b86a
Charles <charl3s.xu@gmail.com>
2020-02-17 15:27:10 +0800

    .

--- src/MinorProjects/yyf/donation.py
+++ src/MinorProjects/yyf/donation.py
@@ -12,9 +12,10 @@ master_table_name, partial_table_name = sys.argv[-2:]
 header_master = ["已筹金额（元）\n含转账", "参与捐赠人次（人次）", "已筹金额（元）\n含转账及线下录入", "参与捐赠人次（人次）.1", "已筹金额（元）\n不含转账及线下录入", "参与捐赠人次（人次）.2", ]
 header_partial = ['筹款量', '捐赠次数', '筹款量.1', '捐赠次数.1', '筹款量.2', '捐赠次数.2', ]
 
-master_table = pd.read_excel(master_table_name, convert_float=False, sheet_name=None, skiprows=1, parse_dates=True, na_values='').popitem()[-1].applymap(lambda x: x.replace('  ', ' ') if type(x) == str else x).dropna(subset=['备案编号'])
+master_table = pd.read_excel(master_table_name, convert_float=False, sheet_name=None, skiprows=0, parse_dates=True, na_values='').popitem()[-1].applymap(lambda x: x.replace('  ', ' ') if type(x) == str else x).dropna(subset=['备案编号'])
 partial_table = pd.read_excel(partial_table_name, convert_float=False, sheet_name=None, skiprows=1).popitem()[-1].applymap(lambda x: x.replace('  ', ' ') if type(x) == str else x).drop(['发起机构.1', '项目名称.1', '发起机构.2', '项目名称.2'], 1)
 master_table = master_table.applymap(lambda x: x.replace('  ', ' ') if type(x) == str else x)
+master_table['目标金额（元）'].fillna('', inplace=True)
 partial_table = partial_table.applymap(lambda x: x.replace('  ', ' ') if type(x) == str else x)
 print('原始主表：', master_table.shape)
 print('原始分表：', partial_table.shape)
@@ -44,14 +45,14 @@ result = pd.merge(left=master_table, right=partial_table, left_on=['慈善组织
 print('按照项目名称与组织合并后总数：', result.shape)
 # merge with cannot match
 abnormal, correct = split_index(result, result.isnull()[['慈善组织', '项目名称', '发起机构']].any(axis=1))
-abnormal_partial, abnormal_master = split_index(abnormal, abnormal.isnull()['备案编号'])
+abnormal_partial, abnormal_master = split_index(abnormal, abnormal.isnull()['序号'])
 result_name = pd.merge(abnormal_master, abnormal_partial, on='项目名称', how='outer')
 print('匹配：', correct.shape, '未匹配', abnormal.shape, '->', '未匹配主表', abnormal_master.shape, '未匹配副表', abnormal_partial.shape)
 
 # merge with cannot match of project name
 print('仅按照项目名称匹配合并后总数：', result_name.shape)
 abnormal_name, correct_name = split_index(result_name, result_name.isnull()[['慈善组织', '项目名称', '发起机构']].any(axis=1))
-abnormal_name_partial, abnormal_name_master = split_index(abnormal_name, abnormal_name.isnull()['备案编号'])
+abnormal_name_partial, abnormal_name_master = split_index(abnormal_name, abnormal_name.isnull()['序号'])
 result_org = pd.merge(abnormal_name_master, abnormal_name_partial, left_on='慈善组织', right_on='发起机构', how='outer')
 print('匹配：', correct_name.shape, '未匹配', abnormal_name.shape, '->', '未匹配主表', abnormal_name_master.shape, '未匹配副表', abnormal_name_partial.shape)
 
